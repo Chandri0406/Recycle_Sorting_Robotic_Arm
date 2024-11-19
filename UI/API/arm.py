f4aeb38d0f6a03a8067d7dba1a5420dd9856a254
Chandri0406 <106561939+Chandri0406@users.noreply.github.com>
2024-11-19 10:49:10 +0200

    new ui

--- UI/API/arm.py
+++
@@ -1,8 +0,0 @@
-#This code is for micropython file
-import machine
-import ubinascii
-
-uart = machine.UART(1, baudrate=9600, tx=machine.Pin(12), rx=machine.Pin(13))
-
-def send_data(data):
-    uart.write(ubinascii.hexlify(data))
